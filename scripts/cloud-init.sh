be8c5d81d136c40af892b4b2dfdb2d4b4d62ace7
Zdeněk Vydra <485514@mail.muni.cz>
2023-03-20 10:53:42 +0100

    Update OPNsense to 23.1

--- scripts/cloud-init.sh
+++ scripts/cloud-init.sh
@@ -8,7 +8,7 @@ pkg install -y xmlstarlet base64
 echo "FreeBSD: { enabled: no }" > /usr/local/etc/pkg/repos/FreeBSD.conf
 pkg update
 
-pkg install -y net/cloud-init os-qemu-guest-agent-1.1
+pkg install -y net/cloud-init os-qemu-guest-agent-1.2
 echo 'qemu_guest_agent_enable="YES"' >> /etc/rc.conf
 echo 'qemu_guest_agent_flags="-d -v -l /var/log/qemu-ga.log"' >> /etc/rc.conf
 echo 'cloudinit_enable="YES"' >> /etc/rc.conf
