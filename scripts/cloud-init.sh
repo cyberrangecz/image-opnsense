d5d3f8c2859eb98b06bb8c6a3e06bacd0084e85e
Zdeněk Vydra <vydra@cshub.cz>
2026-09-11 14:41:32 +0200

    Disable firewall by default, update
    Without disabling firewall, OpenStack metadata service is not reachable

--- scripts/cloud-init.sh
+++ scripts/cloud-init.sh
@@ -2,14 +2,17 @@
 set -e
 
 # Install xmlstarlet and cloud-init
+export IGNORE_OSVERSION=yes
+
 echo "FreeBSD: { enabled: yes }" > /usr/local/etc/pkg/repos/FreeBSD.conf
-pkg-static update
-pkg-static install -y xmlstarlet base64 py311-cloud-init #-24.1.4_4
+pkg-static update -f
+pkg-static install -y xmlstarlet base64 py312-cloud-init #-24.1.4_4
 echo 'cloudinit_enable="YES"' >> /etc/rc.conf
 echo "FreeBSD: { enabled: no }" > /usr/local/etc/pkg/repos/FreeBSD.conf
-pkg-static update
+pkg-static update -f
 
 pkg-static install -y os-qemu-guest-agent #-1.3
 echo 'qemu_guest_agent_enable="YES"' >> /etc/rc.conf
 echo 'qemu_guest_agent_flags="-d -v -l /var/log/qemu-ga.log"' >> /etc/rc.conf
+mkdir -p /usr/local/etc/cloud/
 cp -f /tmp/cloud.cfg /usr/local/etc/cloud/cloud.cfg
--- scripts/fixes.sh
+++ scripts/fixes.sh
@@ -4,14 +4,5 @@ set -e
 # Set link for Ansible to find python
 sudo ln -s /usr/local/bin/python3 /usr/bin/python3
 
-# Setup vtnet1 for DHCP
-xml edit --inplace -d '/opnsense/interfaces/vtnet1' /conf/config.xml
-xml edit --inplace -s '/opnsense/interfaces' -t elem -n vtnet1 /conf/config.xml
-xml edit --inplace -s '/opnsense/interfaces/vtnet1' -t elem -n if -v vtnet1 /conf/config.xml
-xml edit --inplace -s '/opnsense/interfaces/vtnet1' -t elem -n descr -v vtnet1 /conf/config.xml
-xml edit --inplace -s '/opnsense/interfaces/vtnet1' -t elem -n enable -v 1 /conf/config.xml
-xml edit --inplace -s '/opnsense/interfaces/vtnet1' -t elem -n ipaddr -v dhcp /conf/config.xml
-xml edit --inplace -s '/opnsense/interfaces/vtnet1' -t elem -n alias-subnet -v 32 /conf/config.xml
-
 # Reinstall pkg to fix segmentation fault of pkg
 sudo pkg-static install -fy pkg
